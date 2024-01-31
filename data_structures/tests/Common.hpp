49cfc9bb8fbd5553658247bf7966c233d262fc50
saccharineboi <saccharineboi@gmail.com>
2024-01-31 11:16:24 +0400

    Add color to output

--- data_structures/tests/Common.hpp
+++ data_structures/tests/Common.hpp
@@ -1,9 +1,15 @@
 #pragma once
 
 ////////////////////////////////////////
+#define ANSI_COLOR_RED      "\x1b[31m"
+#define ANSI_COLOR_GREEN    "\x1b[32m"
+#define ANSI_COLOR_YELLOW   "\x1b[33m"
+#define ANSI_COLOR_RESET    "\x1b[0m"
+
+////////////////////////////////////////
 #define TEST(EXPR, MESSAGE) \
     if (EXPR) \
-        std::cout << "[OK] "; \
+        std::cout << (ANSI_COLOR_GREEN "[OK]\t" ANSI_COLOR_RESET); \
     else \
-        std::cout << "[ERROR] "; \
-    std::cout << MESSAGE << '\n'
+        std::cout << (ANSI_COLOR_RED "[ERROR]\t" ANSI_COLOR_RESET); \
+    std::cout << (ANSI_COLOR_YELLOW MESSAGE ANSI_COLOR_RESET) << '\n'
